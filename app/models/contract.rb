9d025efed90103ab83160e856c71deb931dba8d7
arnaud boscq <itsmycar@air-de-its.home>
2021-04-13 14:53:56 +0100

    AdminClients Controller with Index Show Create and Destroy Method to manage Clients

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,13 +2,25 @@
 
 class Contract < ApplicationRecord
   validates :start_date, presence: true
-  validates :numero, presence: true, uniqueness: true
+  validates :number, presence: true, uniqueness: true
   has_many :clients_contracts
   has_many :clients, through: :clients_contracts
   has_many :contracts_options
   has_many :options, through: :contracts_options
 
-  scope :pending, -> { where(statut: 'pending') }
-  scope :active, -> { where(statut: 'active') }
-  scope :finished, -> { where(statut: 'finished') }
+  scope :pending, -> { where(status: 'pending') }
+  scope :active, -> { where(status: 'active') }
+  scope :finished, -> { where(status: 'finished') }
+
+  def self.to_json(contract)
+    {
+      number: contract.number,
+      status: contract.status,
+      start_date: contract.start_date,
+      end_date: contract.end_date,
+      clients_email: contract.clients.pluck(:email, :id),
+      options: contract.options
+    }
+    # clients_email could be replace by the full_name of all clients are on the contract
+  end
 end
