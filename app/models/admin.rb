d834b886ba5614c2650c7451741c3b1d9e6ea91d
Arnaud Boscq <boscqarnaud@gmail;com>
2021-04-14 09:21:56 +0100

    Admin No Registerable

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,7 +4,6 @@ class Admin < ActiveRecord::Base
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
